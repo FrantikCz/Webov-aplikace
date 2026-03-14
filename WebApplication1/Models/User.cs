15eaeac4dbfcc10605c53f073cd5d79088d8051a
František Němeček <nemecekf24@spstrutnov.cz>
2026-03-14 18:22:12 +0100

    vše funguje

--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication1.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,15 +1,18 @@
+using WebApplication1.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite("Data Source=users.db"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,7 +20,6 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
